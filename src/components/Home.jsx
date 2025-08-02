b10bc2b6cd35eacf34f0dd761ab5f38ff3f3c9a9
Jenna <healthylifestyleservices@protonmail.com>
2025-08-02 09:53:15 -0600

    🚑 Add missing Home.jsx to fix build error

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", color: "#f2ede9", textAlign: "center" }}>
+      <h1>Welcome to iTERRA™</h1>
+      <p>This is your concierge wellness platform.</p>
+    </div>
+  );
+}
+
